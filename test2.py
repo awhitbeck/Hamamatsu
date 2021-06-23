5e4d22bde379c86e63ecdacf07601bfe701e8b31
TimRamos77 <Timothy.C.Ramos@ttu.edu>
2021-06-22 23:09:16 -0500

    test commit

--- test2.py
+++ test2.py
@@ -1,11 +1,3 @@
 from tabulate import tabulate
 import random
 
-for i in range(self.y):  # APPENDS THE TABLE IN THE MIDDLE CREATING EMPTY BYTES FOR DATA
-    decode[0].append("")
-    decode[1].append("")
-    for n in range(4):
-        decode[0][9 - n + i] = decode[0][8 - n + i]
-        encode[1][9 - n + i] = decode[1][8 - n + i]
-    decode[0][5 + i] = ""
-    decode[1][5 + i] = ""
